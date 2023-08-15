88ad9de4603580e09ce4174dbedcce18cdfcb9ff
erik kjell <freshhhhoi@gmail.com>
2023-08-15 15:00:12 -0500

    Add morse to english

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 
 #Dictionary for morse code translations
 #Codes end with space to account for singular 
-#space needed after letters
+#Space needed after letters too account for character spacing in morse code
 morse_dict = {'a' : '.- ', 'b' : '-... ', 'c' : '-.-. ',
               'd' : '-.. ', 'e' : '. ', 'f' : '..-. ',
               'g' : '--. ', 'h' : '.... ', 'i' : '.. ',
               'j' : '.--- ', 'k' : '-.- ', 'l' : '.-.. ',
               'm' : '-- ', 'n' : '-. ', 'o' : '--- ', 
               'p' : '.--. ', 'q' : '--.- ', 'r' : '.-. ',
-              's' : '...', 't' : '- ', 'u' : '..- ',
+              's' : '... ', 't' : '- ', 'u' : '..- ',
               'v' : '...- ', 'w' : '.-- ', 'x' : '-..- ',
               'y' : '-.-- ', 'z' : '--.. ', '0' : '----- ',
               '1' : '.---- ', '2' : '..--- ', '3' : '...-- ',
@@ -22,7 +22,19 @@ morse_dict = {'a' : '.- ', 'b' : '-... ', 'c' : '-.-. ',
               '+' : '.-.-. ', '-' : '-....- ', '_' : '..--.- ',
               '"' : '.-..-. ', '$' : '...-..- ', '@' : '.--.-. ',
               ' ' : '/ '}
+
+#reverses morse_dict and creates a new reverse_morse_dict sorted by
+#key length in order to translate
+reverse_morse_list = [[value, key] for key, value in morse_dict.items()]
+reverse_morse_list.sort(reverse=True, key = lambda x: len(x[0]))
+reverse_morse_dict = { kv_pair[0] : kv_pair[1] for kv_pair in reverse_morse_list}
+
 original_text = str(input('Enter the text you want translated to morse: ').lower())
 morse_table = str.maketrans(morse_dict)
 
-print(original_text.translate(morse_table))
+translated = original_text.translate(morse_table)
+retranslated = translated
+for key, value in reverse_morse_dict.items():
+    retranslated = retranslated.replace(key, value)
+print(translated)
+print(retranslated)
